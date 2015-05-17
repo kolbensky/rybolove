43a7bba8d4f736b108938456c0db408018f3d24e
nikolayr1 <nikolay5011995@gmail.com>
2015-05-17 17:16:54 +0400

    upgradeALittleBit

--- www/themes/default/assets/js/menu.js
+++ www/themes/default/assets/js/menu.js
@@ -1,14 +1,18 @@
 (function($){
 $(document).ready(function(){
 $('.mainm li.parent').addClass('has-sub')
-
+var filter = $(document).find('#filter');//find filter
     $('.mainm li.has-sub>a').on('click', function(){
         $(this).removeAttr('href');
         var element = $(this).parent('li');
+        
         if (element.hasClass('open')) {
             element.removeClass('open');
             element.find('li').removeClass('open');
             element.find('ul').slideUp(200);
+            if(filter.length == 1){ //if there is a filter on the page
+                filter.css('display','block');
+            }
         }
         else {
             element.addClass('open');
@@ -17,6 +21,9 @@ $('.mainm li.parent').addClass('has-sub')
             element.siblings('li').removeClass('open');
             element.siblings('li').find('li').removeClass('open');
             element.siblings('li').find('ul').slideUp(200);
+            if(filter.length == 1){ //if there is a filter on the page
+                filter.css('display','none');
+            }
         }
     });
 
