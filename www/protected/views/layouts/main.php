9c4c2563711b42c12bc2b38d66ca1941fdcc66bd
nikolayr1 <nikolay5011995@gmail.com>
2015-05-23 23:50:28 +0400

    privateCss2

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -40,11 +40,11 @@
 	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/fonts.css">
 	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/menu.css">
 	<!-- private css-->
-	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/Dasha_H.css">
+	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/dasha_h.css">
 	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/nikolay_r.css">
-	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/Anton_A.css">
-	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/Alexander_A.css">
-	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/Yury_S.css">
+	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/anton_a.css">
+	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/alexander_a.css">
+	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl ?>/assets/css/private_css/yury_s.css">
 	
 	<script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl ?>/assets/js/common.js"></script>
 	<script type="text/javascript" src="<?php echo Yii::app()->theme->baseUrl ?>/assets/js/menu.js"></script>
