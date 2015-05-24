ba0a8527b7aba1cf64afcf6afccf71e8167bac83
dadakax <daharlan@yandex.ru>
2015-05-24 21:05:43 +0300

    fix

--- www/themes/rybolove/views/layouts/main.php
+++ www/themes/rybolove/views/layouts/main.php
@@ -81,10 +81,7 @@
             </div>
         </div>
     </div> <!-- End header area -->
-<<<<<<< HEAD
 
-=======
->>>>>>> 6cc6e7299ff19030276dd95b289af1bd4b23ed4b
     <div class="site-branding-area">
         <div class="container">
             <div class="row">
@@ -93,11 +90,7 @@
                         <img src="<?=$temdir?>img/logo.png" alt="">
                     </div>
                 </div>
-<<<<<<< HEAD
 
-=======
-                
->>>>>>> 6cc6e7299ff19030276dd95b289af1bd4b23ed4b
                 <div class="col-sm-6">
                     <div class="shopping-item">
                         <a href="cart.html">Cart - <span class="cart-amunt">$800</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">5</span></a>
@@ -106,36 +99,9 @@
             </div>
         </div>
     </div> <!-- End site branding area -->
-<<<<<<< HEAD
 
-    <div class="mainmenu-area">
-        <div class="container">
-            <div class="row">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li><a href="shop.html">Shop page</a></li>
-                        <li><a href="single-product.html">Single product</a></li>
-                        <li><a href="cart.html">Cart</a></li>
-                        <li><a href="checkout.html">Checkout</a></li>
-                        <li><a href="#">Category</a></li>
-                        <li><a href="#">Others</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End mainmenu area -->
-=======
->>>>>>> 6cc6e7299ff19030276dd95b289af1bd4b23ed4b
+
+    
     
     <div class="mainmenu-area">
         <div class="container">
