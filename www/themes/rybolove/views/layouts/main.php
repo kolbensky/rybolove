9c9577b406ea9c4c5b33a3d2dd741ad3ad2c2be9
dadakax <daharlan@yandex.ru>
2015-05-30 21:41:40 +0300

    all day all night

--- www/themes/rybolove/views/layouts/main.php
+++ www/themes/rybolove/views/layouts/main.php
@@ -18,6 +18,7 @@
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+	<link href='themes/rybolove/assets/fonts/Decima_Nova_Pro_Light.otf' rel='stylesheet' >
     
     <!-- Bootstrap -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -89,13 +90,21 @@
     <div class="site-branding-area">
         <div class="container">
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-4">
                     <div class="logo">
                         <img src="<?=$temdir?>img/logo.png" alt="">
                     </div>
                 </div>
-
-                <div class="col-sm-6">
+				<div class="col-sm-4">
+					<div class="single-sidebar">
+                       
+						<?php echo CHtml::form($this->createUrl('/store/category/search')) ?>
+					<input type="text" placeholder="Поиск товаров" name="q" id="searchQuery">
+					<button type="submit">Поиск</button>
+						<?php echo CHtml::endForm() ?>
+                    </div>
+				</div>
+                <div class="col-sm-4">
                     <div class="shopping-item">
                         <a href="cart.html">Cart - <span class="cart-amunt">$800</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">5</span></a>
                     </div>
