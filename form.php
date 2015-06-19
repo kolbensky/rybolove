3a8f52311a0a6cc7b7ec53863dda2dc54a6a1128
dumbik <dumbik77@gmail.com>
2015-06-19 14:53:26 +0300

    test download

--- form.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$email_text = 'Имя: '.$_POST['form-name'].'      Телефон: '.$_POST['form-phone'].'      '.$_POST['form-textarea'];
-$subject = 'Заявка';
-mail('dumbik77@gmail.com', $subject, $email_text);
-
-?>
